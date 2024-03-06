7ce8e62ec86078159409920975b8cdd27e1bd5a7
karishmas6 <carishmashukla28@gmail.com>
2024-03-07 04:01:56 +0530

    feat: remove unwanted code

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,10 +12,10 @@ const App = () => (
       },
     }}
   >
-    <Space>
+   
       <Button type="primary">Primary</Button>
       <Button>Default</Button>
-    </Space>
+    
   <BrowserRouter>
     <Topbar />
     <Sidebar />
