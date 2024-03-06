b7e18c6a865f897af7c5685112ae15ffc09b5773
karishmas6 <carishmashukla28@gmail.com>
2024-03-07 03:21:21 +0530

    feat: use Sidebar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,11 @@
 import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar";
 import { BrowserRouter } from 'react-router-dom'
 
 const App = () => (
   <BrowserRouter>
     <Topbar />
+    <Sidebar />
   </BrowserRouter>
 );
 
