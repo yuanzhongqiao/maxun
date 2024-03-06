31b76e8c830392902c7f9d169d13bf794a20dbf9
karishmas6 <carishmashukla28@gmail.com>
2024-03-07 03:28:39 +0530

    chore: lint

--- ui/src/components/Sidebar/index.tsx
+++ ui/src/components/Sidebar/index.tsx
@@ -4,34 +4,34 @@ import { Link } from "react-router-dom";
 const { Sider } = Layout;
 
 const Sidebar = () => {
-  return (
-    <Sider
-      className="h-screen bg-white text-gray-800 fixed top-16 left-0 flex flex-col justify-between"
-      width={250} 
-    >
-      <div className="flex flex-col justify-between h-full"> 
-        <Menu mode="vertical">
-          <Menu.Item key="1">
-            <Link to="/bots">Bots</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/subscription">Subscription</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/api">API</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/workflow">Workflow</Link>
-          </Menu.Item>
-        </Menu>
-        <div className="flex items-center py-4 px-6">
-          {/* Add  logo image here */}
-          <img src="your_logo.png" alt="Maxun logo" className="h-8 w-auto mr-4" />
-          <span className="text-xl font-bold">Maxun</span>
-        </div>
-      </div>
-    </Sider>
-  );
+    return (
+        <Sider
+            className="h-screen bg-white text-gray-800 fixed top-16 left-0 flex flex-col justify-between"
+            width={250}
+        >
+            <div className="flex flex-col justify-between h-full">
+                <Menu mode="vertical">
+                    <Menu.Item key="1">
+                        <Link to="/bots">Bots</Link>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Link to="/subscription">Subscription</Link>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Link to="/api">API</Link>
+                    </Menu.Item>
+                    <Menu.Item key="4">
+                        <Link to="/workflow">Workflow</Link>
+                    </Menu.Item>
+                </Menu>
+                <div className="flex items-center py-4 px-6">
+                    {/* Add  logo image here */}
+                    <img src="your_logo.png" alt="Maxun logo" className="h-8 w-auto mr-4" />
+                    <span className="text-xl font-bold">Maxun</span>
+                </div>
+            </div>
+        </Sider>
+    );
 };
 
 export default Sidebar;
