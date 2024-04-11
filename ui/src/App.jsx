d716e09355b8ba37b70f2124139bd6c1bec0b65a
karishmas6 <carishmashukla28@gmail.com>
2024-04-11 14:14:26 +0530

    refactor: remove typescript

---
+++ ui/src/App.jsx
@@ -0,0 +1,24 @@
+import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar";
+import Scraper from "./components/Scraper";
+import { ConfigProvider } from 'antd';
+import { BrowserRouter } from 'react-router-dom'
+
+const App = () => (
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#ff00c3',
+        borderRadius: 2,
+      },
+    }}
+  >
+    <BrowserRouter>
+      <Topbar />
+      {/* <Sidebar />  */}
+      <Scraper />
+    </BrowserRouter>
+  </ConfigProvider>
+);
+
+export default App;
