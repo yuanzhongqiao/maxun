8ba8a6927c1e9f53003a68ec59f84f181255793f
karishmas6 <carishmashukla28@gmail.com>
2024-04-20 00:51:38 +0530

    chore: lint

--- scraper/src/scraper.js
+++ scraper/src/scraper.js
@@ -6,22 +6,20 @@ async function scrapeData(url, selectors) {
         requestHandler: async ({ page, request }) => {
             await page.goto(url);
             console.log('Received selectors:', selectors);
-  
             for (const selector of selectors) {
                 const elementData = await page.$$eval(selector, elements => elements.map(el => el.textContent.trim()));
                 scrapedData.push(...elementData);
             }
-  
             console.log('Scraped data:', scrapedData);
         },
     },
-    new Configuration({
-        persistStorage: false,
-    }));
+        new Configuration({
+            persistStorage: false,
+        }));
 
-    await crawler.run([ url ]);
+    await crawler.run([url]);
     return scrapedData;
-   
+
 }
 
 export default scrapeData;
