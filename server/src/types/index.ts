a1a42a7f0aecc5042317334e284a8d06ddca01c2
karishmas6 <carishmashukla28@gmail.com>
2024-06-01 11:35:18 +0530

    feat(ts): interface RemoteBrowserOptions

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -10,3 +10,18 @@ export interface InterpreterSettings {
     debug: boolean;
     params?: any;
 }
+
+
+/**
+ * Options for the {@link BrowserManagement.launch} method.
+ * Wraps the Playwright's launchOptions and adds an extra browser option.
+ * The browser option determines which browser to launch as Playwright
+ * supports multiple browsers. (chromium, firefox, webkit)
+ *  -- Possible expansion for the future of the browser recorder --
+ * @category Types
+ */
+export interface RemoteBrowserOptions {
+    browser: BrowserType
+    launchOptions: LaunchOptions
+};
+
