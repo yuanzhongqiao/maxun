8ae020b93b3e27d9fd67f8b4947374c0e730583c
karishmas6 <carishmashukla28@gmail.com>
2024-06-01 08:56:52 +0530

    chore: BrowserPoolInfo comments

--- server/src/browser-management/classes/BrowserPool.ts
+++ server/src/browser-management/classes/BrowserPool.ts
@@ -1,11 +1,19 @@
 import { RemoteBrowser } from "./RemoteBrowser";
 import logger from "../../logger";
 
-
+/**
+ * @category Types
+ */
 interface BrowserPoolInfo {
-   
+    /**
+     * The instance of remote browser.
+     */
     browser: RemoteBrowser,
-    
+    /**
+     * States if the browser's instance is being actively used.
+     * Helps to persist the progress on the frontend when the application has been reloaded.
+     * @default false
+     */
     active: boolean,
 }
 
