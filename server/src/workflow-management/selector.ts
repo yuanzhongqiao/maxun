0dd469eae637ea4bccbcb3f273a2bf776e3e7460
karishmas6 <carishmashukla28@gmail.com>
2024-06-05 23:39:55 +0530

    feat: helper fxn to check null vals

--- server/src/workflow-management/selector.ts
+++ server/src/workflow-management/selector.ts
@@ -407,7 +407,12 @@ export const getSelectors = async (page: Page, coordinates: Coordinates) => {
          return null;
        }
 
+       function notEmpty<T>(value: T | null | undefined): value is T {
+         return value !== null && value !== undefined;
+       }
+
        
+
        
 };
 
