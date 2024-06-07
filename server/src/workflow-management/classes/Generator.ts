665c5a2cf1184987a7a1ecd41194351be5f51f3d
karishmas6 <carishmashukla28@gmail.com>
2024-06-08 00:15:52 +0530

    feat: add pair to worflow

--- server/src/workflow-management/classes/Generator.ts
+++ server/src/workflow-management/classes/Generator.ts
@@ -325,5 +325,18 @@ export class WorkflowGenerator {
     }
   };
 
+ 
+  public addPairToWorkflow = (index: number, pair: WhereWhatPair) => {
+    if (index === this.workflowRecord.workflow.length) {
+      this.workflowRecord.workflow.unshift(pair);
+      logger.log('debug', `pair ${index}: Added to workflow file.`);
+    } else if (index < this.workflowRecord.workflow.length && index >= 0) {
+      this.workflowRecord.workflow.splice(
+        this.workflowRecord.workflow.length - index , 0, pair);
+    } else {
+      logger.log('error', `Add pair ${index}: Index out of range.`);
+    }
+  };
+
   
 }
