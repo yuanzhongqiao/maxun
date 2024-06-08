527dbe7dc1a33b60b751af6ec676a9fa7da6b939
karishmas6 <carishmashukla28@gmail.com>
2024-06-09 00:43:46 +0530

    feat: get record linked to RB instance

--- server/src/routes/workflow.ts
+++ server/src/routes/workflow.ts
@@ -16,3 +16,17 @@ router.all('/', (req, res, next) => {
   logger.log('debug',`The workflow API was invoked: ${req.url}`)
   next() // pass control to the next handler
 })
+
+/**
+ * GET endpoint for a recording linked to a remote browser instance.
+ * returns session's id
+ */
+router.get('/:browserId', (req, res) => {
+  const activeBrowser = browserPool.getRemoteBrowser(req.params.browserId);
+  let workflowFile = null;
+  if (activeBrowser && activeBrowser.generator) {
+    workflowFile = activeBrowser.generator.getWorkflowFile();
+  }
+  return res.send(workflowFile);
+});
+
