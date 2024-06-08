be07e5a40e3446013410deb05e7eb4c75df8ce3f
karishmas6 <carishmashukla28@gmail.com>
2024-06-08 20:56:15 +0530

    docs: read file

--- server/src/workflow-management/storage.ts
+++ server/src/workflow-management/storage.ts
@@ -5,6 +5,12 @@
 import fs from 'fs';
 import * as path from "path";
 
+/**
+ * Reads a file from path and returns its content as a string.
+ * @param path The path to the file.
+ * @returns {Promise<string>}
+ * @category WorkflowManagement-Storage
+ */
 export const readFile = (path: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
