7274753728077c9db1900b28072f254c16d9bb9f
karishmas6 <carishmashukla28@gmail.com>
2024-06-08 22:50:12 +0530

    feat: helper fxn for user input

--- server/src/browser-management/inputHandlers.ts
+++ server/src/browser-management/inputHandlers.ts
@@ -13,3 +13,34 @@ import { Page } from "playwright";
 import { throttle } from "../../../src/helpers/inputHelpers";
 import { CustomActions } from "../../../src/shared/types";
 
+
+const handleWrapper = async (
+  handleCallback: (
+    generator: WorkflowGenerator,
+    page: Page,
+    args?: any
+  ) => Promise<void>,
+  args?: any
+) => {
+    const id = browserPool.getActiveBrowserId();
+    if (id) {
+        const activeBrowser = browserPool.getRemoteBrowser(id);
+        if (activeBrowser?.interpreter.interpretationInProgress() && !activeBrowser.interpreter.interpretationIsPaused) {
+            logger.log('debug', `Ignoring input, while interpretation is in progress`);
+            return;
+        }
+        const currentPage = activeBrowser?.getCurrentPage();
+        if (currentPage && activeBrowser) {
+            if (args) {
+                await handleCallback(activeBrowser.generator, currentPage, args);
+            } else {
+                await handleCallback(activeBrowser.generator, currentPage);
+            }
+        } else {
+            logger.log('warn', `No active page for browser ${id}`);
+        }
+    } else {
+        logger.log('warn', `No active browser for id ${id}`);
+    }
+}
+
