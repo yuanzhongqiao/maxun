505d2652b0bbd6a20cc467d4636bc8ed699599eb
karishmas6 <carishmashukla28@gmail.com>
2024-06-09 01:08:34 +0530

    feat: get active workflow params

--- src/api/workflow.ts
+++ src/api/workflow.ts
@@ -17,3 +17,17 @@ export const getActiveWorkflow = async(id: string) : Promise<WorkflowFile> => {
   }
 };
 
+export const getParamsOfActiveWorkflow = async(id: string) : Promise<string[]|null> => {
+  try {
+    const response = await axios.get(`http://localhost:8080/workflow/params/${id}`)
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error('Something went wrong when fetching the parameters of the recorded workflow');
+    }
+  } catch(error: any) {
+    console.log(error);
+    return null;
+  }
+};
+
