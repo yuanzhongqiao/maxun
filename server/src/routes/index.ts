d5576a146310c5aad21b799b491a85ab7bd32840
karishmas6 <carishmashukla28@gmail.com>
2024-06-08 23:15:32 +0530

    chore: lint

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,6 @@
-import { router as record}  from './record';
-import { router as workflow} from './workflow';
-import { router as storage} from './storage';
+import { router as record } from './record';
+import { router as workflow } from './workflow';
+import { router as storage } from './storage';
 
 export {
     record,
