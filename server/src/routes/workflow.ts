e03b482a119779ce7236ff78099fa454dac8c4b7
karishmas6 <carishmashukla28@gmail.com>
2024-06-09 00:42:51 +0530

    feat: init workflow router

---
+++ server/src/routes/workflow.ts
@@ -0,0 +1,11 @@
+/**
+ * RESTful API endpoints handling currently generated workflow management.
+ */
+
+import { Router } from 'express';
+import logger from "../logger";
+import { browserPool } from "../server";
+import { readFile } from "../workflow-management/storage";
+
+export const router = Router();
+
