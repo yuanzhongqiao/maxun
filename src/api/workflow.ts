1af5e4ce731e1d286c34ffd07eadd472006cb34d
karishmas6 <carishmashukla28@gmail.com>
2024-06-09 01:09:10 +0530

    feat: add workflow pair

--- src/api/workflow.ts
+++ src/api/workflow.ts
@@ -45,3 +45,19 @@ export const deletePair = async(index: number): Promise<WorkflowFile> => {
   }
 };
 
+export const AddPair = async(index: number, pair: WhereWhatPair): Promise<WorkflowFile> => {
+  try {
+    const response = await axios.post(`http://localhost:8080/workflow/pair/${index}`, {
+      pair,
+    }, {headers: {'Content-Type': 'application/json'}});
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error('Something went wrong when fetching an updated workflow');
+    }
+  } catch (error: any) {
+    console.log(error);
+    return emptyWorkflow;
+  }
+};
+
