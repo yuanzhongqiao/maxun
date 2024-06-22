2e6f47069f3cc9774380328b3b8c1a369c0ba85f
karishmas6 <carishmashukla28@gmail.com>
2024-06-22 20:01:06 +0530

    chore: lint

--- src/components/molecules/action-settings/clickOnCoordinates.tsx
+++ src/components/molecules/action-settings/clickOnCoordinates.tsx
@@ -4,7 +4,7 @@ import { WarningText } from '../../atoms/texts';
 import InfoIcon from "@mui/icons-material/Info";
 
 export const ClickOnCoordinatesSettings = forwardRef((props, ref) => {
-  const [settings, setSettings] = React.useState<number[]>([0,0]);
+  const [settings, setSettings] = React.useState<number[]>([0, 0]);
   useImperativeHandle(ref, () => ({
     getSettings() {
       return settings;
@@ -14,7 +14,7 @@ export const ClickOnCoordinatesSettings = forwardRef((props, ref) => {
   return (
     <Stack direction="column">
       <TextField
-        sx={{marginLeft: '15px', marginRight: '15px'}}
+        sx={{ marginLeft: '15px', marginRight: '15px' }}
         type="number"
         label="X"
         onChange={(e) => setSettings(prevState => ([Number(e.target.value), prevState[1]]))}
@@ -22,7 +22,7 @@ export const ClickOnCoordinatesSettings = forwardRef((props, ref) => {
         defaultValue={settings[0]}
       />
       <TextField
-        sx={{margin: '15px'}}
+        sx={{ margin: '15px' }}
         type="number"
         label="Y"
         onChange={(e) => setSettings(prevState => ([prevState[0], Number(e.target.value)]))}
@@ -30,7 +30,7 @@ export const ClickOnCoordinatesSettings = forwardRef((props, ref) => {
         defaultValue={settings[1]}
       />
       <WarningText>
-        <InfoIcon color='warning'/>
+        <InfoIcon color='warning' />
         The click function will click on the given coordinates.
         You need to put the coordinates by yourself.
       </WarningText>
