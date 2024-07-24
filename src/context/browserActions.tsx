5576aa26b032c62f07f4d429ffec866269c9a9b8
karishmas6 <carishmashukla28@gmail.com>
2024-07-24 19:30:22 +0530

    feat: action context & hook

--- src/context/browserActions.tsx
+++ src/context/browserActions.tsx
@@ -8,3 +8,13 @@ interface ActionContextType {
     resetActions: () => void;
 }
 
+const ActionContext = createContext<ActionContextType | undefined>(undefined);
+
+export const useActionContext = (): ActionContextType => {
+    const context = useContext(ActionContext);
+    if (context === undefined) {
+        throw new Error('useActionContext must be used within an ActionProvider');
+    }
+    return context;
+};
+
