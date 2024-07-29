db21e9a40c07bccf4aa42b20c900858b429f6a09
karishmas6 <carishmashukla28@gmail.com>
2024-07-29 20:04:34 +0530

    chore: -rm script

--- src/components/molecules/action-settings/index.ts
+++ src/components/molecules/action-settings/index.ts
@@ -2,7 +2,6 @@ import { ScrollSettings } from './scroll';
 import { ScreenshotSettings } from "./screenshot";
 import { ScrapeSettings } from "./scrape";
 import { ScrapeSchemaSettings } from "./scrapeSchema";
-import { ScriptSettings } from "./script";
 import { EnqueueLinksSettings } from "./enqueueLinks";
 import { ClickOnCoordinatesSettings } from "./clickOnCoordinates";
 
