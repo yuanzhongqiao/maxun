71d1b66399a3f3469945bd55d5d811a48d3c1504
karishmas6 <carishmashukla28@gmail.com>
2024-07-31 05:39:55 +0530

    feat(core): init interpreter class + constructor

--- mx-interpreter/interpret.ts
+++ mx-interpreter/interpret.ts
@@ -29,3 +29,54 @@ interface InterpreterOptions {
   }>
 }
 
+/**
+ * Class for running the Smart Workflows.
+ */
+export default class Interpreter extends EventEmitter {
+  private workflow: Workflow;
+
+  private initializedWorkflow: Workflow | null;
+
+  private options: InterpreterOptions;
+
+  private concurrency : Concurrency;
+
+  private stopper : Function | null = null;
+
+  private log : typeof log;
+
+  constructor(workflow: WorkflowFile, options?: Partial<InterpreterOptions>) {
+    super();
+    this.workflow = workflow.workflow;
+    this.initializedWorkflow = null;
+    this.options = {
+      maxRepeats: 5,
+      maxConcurrency: 5,
+      serializableCallback: (data) => { log(JSON.stringify(data), Level.WARN); },
+      binaryCallback: () => { log('Received binary data, thrashing them.', Level.WARN); },
+      debug: false,
+      debugChannel: {},
+      ...options,
+    };
+    this.concurrency = new Concurrency(this.options.maxConcurrency);
+    this.log = (...args) => log(...args);
+
+    const error = Preprocessor.validateWorkflow(workflow);
+    if (error) {
+      throw (error);
+    }
+
+    if (this.options.debugChannel?.debugMessage) {
+      const oldLog = this.log;
+      // @ts-ignore
+      this.log = (...args: Parameters<typeof oldLog>) => {
+        if (args[1] !== Level.LOG) {
+          this.options.debugChannel.debugMessage!(typeof args[0] === 'string' ? args[0] : args[0].message);
+        }
+        oldLog(...args);
+      };
+    }
+  }
+
+  
+}
