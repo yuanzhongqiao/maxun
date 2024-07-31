29b365b7143cd5eeb588285e6fe46a851775dce2
karishmas6 <carishmashukla28@gmail.com>
2024-07-31 05:36:34 +0530

    feat(core): extract params from workflow

--- mx-interpreter/preprocessor.ts
+++ mx-interpreter/preprocessor.ts
@@ -46,5 +46,28 @@ export default class Preprocessor {
     return error;
   }
 
+  /**
+* Extracts parameter names from the workflow.
+* @param {WorkflowFile} workflow The given workflow
+* @returns {String[]} List of parameters' names.
+*/
+  static getParams(workflow: WorkflowFile) : string[] {
+    const getParamsRecurse = (object : any) : string[] => {
+      if (typeof object === 'object') {
+        // Recursion base case
+        if (object.$param) {
+          return [object.$param];
+        }
+
+        // Recursion general case
+        return Object.values(object)
+          .reduce((p: string[], v : any) : string[] => [...p, ...getParamsRecurse(v)], []);
+      }
+      return [];
+    };
+
+    return getParamsRecurse(workflow.workflow);
+  }
+
  
 }
