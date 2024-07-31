c7b9cbd0badad7d3ca18591db6bd23a3ebd1dddb
karishmas6 <carishmashukla28@gmail.com>
2024-07-31 05:39:13 +0530

    feat(core): interpreter options

---
+++ mx-interpreter/interpret.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-await-in-loop, no-restricted-syntax */
+import { Page, PageScreenshotOptions } from 'playwright';
+import path from 'path';
+
+import { EventEmitter } from 'events';
+import {
+  Where, What, PageState, Workflow, WorkflowFile,
+  ParamType, SelectorArray, CustomFunctions,
+} from './types/workflow';
+
+import { operators, meta } from './types/logic';
+import { arrayToObject } from './utils/utils';
+import Concurrency from './utils/concurrency';
+import Preprocessor from './preprocessor';
+import log, { Level } from './utils/logger';
+
+/**
+ * Defines optional intepreter options (passed in constructor)
+ */
+interface InterpreterOptions {
+  maxRepeats: number;
+  maxConcurrency: number;
+  serializableCallback: (output: any) => (void | Promise<void>);
+  binaryCallback: (output: any, mimeType: string) => (void | Promise<void>);
+  debug: boolean;
+  debugChannel: Partial<{
+    activeId: Function,
+    debugMessage: Function,
+  }>
+}
+
