fe97858ad8df19b1592bc74608aed48be12d3d4a
karishmas6 <carishmashukla28@gmail.com>
2024-08-05 23:33:33 +0530

    fix: remove enqueueLinks import

--- src/components/molecules/action-settings/index.ts
+++ src/components/molecules/action-settings/index.ts
@@ -2,12 +2,11 @@ import { ScrollSettings } from './scroll';
 import { ScreenshotSettings } from "./screenshot";
 import { ScrapeSettings } from "./scrape";
 import { ScrapeSchemaSettings } from "./scrapeSchema";
-import { EnqueueLinksSettings } from "./enqueueLinks";
 
 export {
   ScrollSettings,
   ScreenshotSettings,
   ScrapeSettings,
   ScrapeSchemaSettings,
-  EnqueueLinksSettings,
+  ,
 };
