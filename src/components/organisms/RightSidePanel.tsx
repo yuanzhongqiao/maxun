eb4b71db2c5568269dd12c1b7981e3329903349f
karishmas6 <carishmashukla28@gmail.com>
2024-08-06 03:37:41 +0530

    chore: lint

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -136,9 +136,9 @@ export const RightSidePanel = () => {
                       )}
                     </>
                   ) : (
-                      step.type === 'screenshot' && (
-                        <Typography>{`Take ${step.fullPage ? 'Fullpage' : 'Visible Part'} Screenshot`}</Typography>
-                      )
+                    step.type === 'screenshot' && (
+                      <Typography>{`Take ${step.fullPage ? 'Fullpage' : 'Visible Part'} Screenshot`}</Typography>
+                    )
                   )
                 }
               </Box>
