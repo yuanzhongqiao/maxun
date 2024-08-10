45df4655ae88a199be0827edc3f1f80f5222e107
karishmas6 <carishmashukla28@gmail.com>
2024-08-10 21:25:17 +0530

    chore: lint

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -82,7 +82,7 @@ export const RightSidePanel = () => {
 
 
   const getListSettingsObject = useCallback(() => {
-    let settings: { listSelector?: string; fields?: Record<string, { selector: string; tag?: string; [key: string]: any }> } = {};
+    let settings: { listSelector?: string; fields?: Record<string, { selector: string; tag?: string;[key: string]: any }> } = {};
 
     browserSteps.forEach(step => {
       if (step.type === 'list' && step.listSelector && Object.keys(step.fields).length > 0) {
@@ -100,7 +100,7 @@ export const RightSidePanel = () => {
         settings = {
           listSelector: step.listSelector,
           fields: fields
-      };
+        };
 
       }
     });
