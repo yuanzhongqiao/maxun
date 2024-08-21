46c5561208e54e943637b69b3f9528d2b6dc094e
karishmas6 <carishmashukla28@gmail.com>
2024-08-21 22:07:32 +0530

    feat: make button size medium

--- src/components/organisms/BrowserWindow.tsx
+++ src/components/organisms/BrowserWindow.tsx
@@ -241,7 +241,7 @@ export const BrowserWindow = () => {
                         <div>
                             <h2>Select Attribute</h2>
                             {attributeOptions.map((option) => (
-                                <Button variant="contained" key={option.value} onClick={() => handleAttributeSelection(option.value)}>
+                                <Button variant="contained" size="medium" key={option.value} onClick={() => handleAttributeSelection(option.value)}>
                                     {option.label}
                                 </Button>
                             ))}
