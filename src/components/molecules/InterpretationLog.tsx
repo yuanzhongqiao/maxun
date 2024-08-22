ee940de7abcc5550f0a660c82b7529652323025d
karishmas6 <carishmashukla28@gmail.com>
2024-08-22 05:49:30 +0530

    feat: set width to 100%

--- src/components/molecules/InterpretationLog.tsx
+++ src/components/molecules/InterpretationLog.tsx
@@ -67,7 +67,7 @@ export const InterpretationLog = () => {
 
   return (
     <div>
-      <button onClick={toggleDrawer(true)} style={{ color: 'white', background: '#3f4853', border: 'none', padding: '10px 20px' }}>
+      <button onClick={toggleDrawer(true)} style={{ color: 'white', background: '#3f4853', border: 'none', padding: '10px 20px', width: '100%' }}>
         Interpretation Log
       </button>
       <SwipeableDrawer
