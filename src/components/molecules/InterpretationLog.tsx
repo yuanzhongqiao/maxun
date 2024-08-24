5e70acec630e0d7a08e264f3364149ebd4bc5fd8
karishmas6 <carishmashukla28@gmail.com>
2024-08-24 09:59:19 +0530

    chore: lint

--- src/components/molecules/InterpretationLog.tsx
+++ src/components/molecules/InterpretationLog.tsx
@@ -113,26 +113,26 @@ export const InterpretationLog = () => {
           {
             getList ? (
               <>
-              <p>What is the maximum number of rows you want to extract?</p>
-              <FormControl>
-      <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-        <FormControlLabel value="other" control={<Radio />} label="Other" />
-        <FormControlLabel
-          value="disabled"
-          disabled
-          control={<Radio />}
-          label="other"
-        />
-      </RadioGroup>
-    </FormControl>
-    </>
+                <p>What is the maximum number of rows you want to extract?</p>
+                <FormControl>
+                  <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                  >
+                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    <FormControlLabel value="other" control={<Radio />} label="Other" />
+                    <FormControlLabel
+                      value="disabled"
+                      disabled
+                      control={<Radio />}
+                      label="other"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </>
             ) : null
           }
           <div style={{ float: "left", clear: "both" }}
