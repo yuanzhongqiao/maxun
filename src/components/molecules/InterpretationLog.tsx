a67c98e35eca321e8aad4c0e0482fa50df731e09
karishmas6 <carishmashukla28@gmail.com>
2024-08-25 23:49:10 +0530

    feat: make table heading sticky

--- src/components/molecules/InterpretationLog.tsx
+++ src/components/molecules/InterpretationLog.tsx
@@ -149,7 +149,7 @@ export const InterpretationLog = () => {
                 </FormControl>
 
                 <TableContainer component={Paper}>
-                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <Table sx={{ minWidth: 650 }} stickyHeader aria-label="simple table">
                     <TableHead>
                       <TableRow>
                         <TableCell>Dessert (100g serving)</TableCell>
