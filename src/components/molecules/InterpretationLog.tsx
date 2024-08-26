4f297423101005de2dc8b85f9bc4ec89468167c9
karishmas6 <carishmashukla28@gmail.com>
2024-08-26 20:06:29 +0530

    feat: use h4 for form label

--- src/components/molecules/InterpretationLog.tsx
+++ src/components/molecules/InterpretationLog.tsx
@@ -171,7 +171,7 @@ export const InterpretationLog = () => {
                 </TableContainer>
 
                 <FormControl>
-                  <FormLabel>What is the maximum number of rows you want to extract?</FormLabel>
+                  <FormLabel><h4>What is the maximum number of rows you want to extract?</h4></FormLabel>
                   <RadioGroup row>
                     <FormControlLabel value="10" control={<Radio />} label="10" />
                     <FormControlLabel value="100" control={<Radio />} label="100" />
