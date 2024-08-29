528d81958b908d119b905ea116cbdbd748c0eb67
karishmas6 <carishmashukla28@gmail.com>
2024-08-30 02:53:23 +0530

    fix: do not allow empty strings

--- src/components/organisms/BrowserWindow.tsx
+++ src/components/organisms/BrowserWindow.tsx
@@ -222,7 +222,7 @@ export const BrowserWindow = () => {
                         id: Date.now(),
                         type: 'text',
                         label: `Label ${Object.keys(fields).length + 1}`,
-                        data: selectedElement.info?.innerText || '',
+                        data: selectedElement.info?.innerText,
                         selectorObj: {
                             selector: selectedElement.selector,
                             tag: selectedElement.info?.tagName,
