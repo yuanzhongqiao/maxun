3899e2e85c384925b4cb86eb9fe73672a545b31b
karishmas6 <carishmashukla28@gmail.com>
2024-08-29 23:04:36 +0530

    fix: missing import for text field

--- src/components/molecules/InterpretationLog.tsx
+++ src/components/molecules/InterpretationLog.tsx
@@ -3,7 +3,7 @@ import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Typography from '@mui/material/Typography';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
