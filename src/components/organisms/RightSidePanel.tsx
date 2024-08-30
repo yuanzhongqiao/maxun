d3fe5ce2eda9202bdb455995b97a8d129d7a56e7
karishmas6 <carishmashukla28@gmail.com>
2024-08-31 03:32:43 +0530

    chore: lint

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -88,10 +88,10 @@ export const RightSidePanel = () => {
 
 
   const getListSettingsObject = useCallback(() => {
-    let settings: { 
-      listSelector?: string; 
-      fields?: Record<string, { selector: string; tag?: string;[key: string]: any }>;
-      pagination?: { type: string; selector?: string } 
+    let settings: {
+      listSelector?: string;
+      fields?: Record<string, { selector: string; tag?: string; [key: string]: any }>;
+      pagination?: { type: string; selector?: string }
     } = {};
 
     browserSteps.forEach(step => {
