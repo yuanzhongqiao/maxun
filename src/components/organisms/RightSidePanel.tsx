af552f2a116e1a01698ad3e088ed1cedeae86afd
karishmas6 <carishmashukla28@gmail.com>
2024-09-01 01:08:24 +0530

    feat: pass selectedPaginationSetiing & paginationSelector in dependency array

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -122,7 +122,7 @@ export const RightSidePanel = () => {
     });
 
     return settings;
-  }, [browserSteps]);
+  }, [browserSteps, selectedPaginationSetting, paginationSelector]);
 
 
   const stopCaptureAndEmitGetListSettings = useCallback(() => {
