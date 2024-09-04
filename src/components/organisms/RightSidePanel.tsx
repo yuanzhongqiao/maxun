eaaaec7b1ad6fa2ac940367f34a0d8898709af24
karishmas6 <carishmashukla28@gmail.com>
2024-09-04 05:38:24 +0530

    chore: -rm useEffect

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Button, Paper, Box, TextField } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
