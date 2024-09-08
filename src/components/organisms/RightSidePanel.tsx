9c6720ecb911f92668a338384f15c29764043dcc
karishmas6 <carishmashukla28@gmail.com>
2024-09-08 12:20:48 +0530

    feat(wip): get limit context

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -38,7 +38,7 @@ export const RightSidePanel: React.FC<RightSidePanelProps> = ({ onFinishCapture 
   const [customLimit, setCustomLimit] = useState<string>('');
 
   const { lastAction, notify } = useGlobalInfoStore();
-  const { getText, startGetText, stopGetText, getScreenshot, startGetScreenshot, stopGetScreenshot, paginationMode, getList, startGetList, stopGetList, startPaginationMode, stopPaginationMode, paginationType, updatePaginationType } = useActionContext();
+  const { getText, startGetText, stopGetText, getScreenshot, startGetScreenshot, stopGetScreenshot, paginationMode, getList, startGetList, stopGetList, startPaginationMode, stopPaginationMode, paginationType, updatePaginationType, limitMode, limitType, customLimit, updateLimitType,  updateCustomLimit, stopLimitMode } = useActionContext();
   const { browserSteps, updateBrowserTextStepLabel, deleteBrowserStep, addScreenshotStep } = useBrowserSteps();
   const { socket } = useSocketStore();
 
