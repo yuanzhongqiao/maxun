f616a9038daf4ff93ac3f06348112036edb65749
karishmas6 <carishmashukla28@gmail.com>
2024-09-09 12:16:51 +0530

    feat: wrap app in browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { GlobalInfoProvider } from "./context/globalInfo";
 import { PageWrapper } from "./pages/PageWrappper";
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <GlobalInfoProvider>
-        <PageWrapper />
-      </GlobalInfoProvider>
-    </BrowserRouter>
+    <GlobalInfoProvider>
+      <PageWrapper />
+    </GlobalInfoProvider>
   );
 }
 
