e722c6308fed0f47725eb036bfbb8af9f83a40de
karishmas6 <carishmashukla28@gmail.com>
2024-09-10 12:07:44 +0530

    feat(wip): pass handleScheduleRecording as prop (temp)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -102,6 +102,7 @@ export const MainPage = ({ handleEditRecording }: MainPageProps) => {
           handleEditRecording={handleEditRecording}
           handleRunRecording={handleRunRecording}
           setFileName={setFileName}
+          handleScheduleRecording={() => {}}
         />;
       case 'runs':
         return <Runs
