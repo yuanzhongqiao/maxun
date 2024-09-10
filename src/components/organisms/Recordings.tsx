8e97cebd7d4050381b9cd0cb1276597cb1e26c9e
karishmas6 <carishmashukla28@gmail.com>
2024-09-10 12:09:53 +0530

    feat(wip): handleScheduleRecording

--- src/components/organisms/Recordings.tsx
+++ src/components/organisms/Recordings.tsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import { RecordingsTable } from "../molecules/RecordingsTable";
 import { Grid } from "@mui/material";
 import { RunSettings, RunSettingsModal } from "../molecules/RunSettings";
+import { ScheduleSettings, ScheduleSettingsModal } from "../molecules/ScheduleSettings";
 
 interface RecordingsProps {
   handleEditRecording: (fileName: string) => void;
   handleRunRecording: (settings: RunSettings) => void;
+  handleScheduleRecording: (settings: RunSettings) => void;
   setFileName: (fileName: string) => void;
-
 }
 
-export const Recordings = ({ handleEditRecording, handleRunRecording, setFileName }: RecordingsProps) => {
+export const Recordings = ({ handleEditRecording, handleRunRecording, setFileName, handleScheduleRecording }: RecordingsProps) => {
   const [runSettingsAreOpen, setRunSettingsAreOpen] = useState(false);
+  const [scheduleSettingsAreOpen, setScheduleSettingsAreOpen] = useState(false);
   const [params, setParams] = useState<string[]>([]);
 
   const handleSettingsAndRun = (fileName: string, params: string[]) => {
@@ -25,9 +27,26 @@ export const Recordings = ({ handleEditRecording, handleRunRecording, setFileNam
     }
   }
 
+  const handleSettingsAndSchedule = (fileName: string, params: string[]) => {
+    if (params.length === 0) {
+      setScheduleSettingsAreOpen(true);
+      setFileName(fileName);
+    } else {
+      setParams(params);
+      setScheduleSettingsAreOpen(true);
+      setFileName(fileName);
+    }
+  }
+
   const handleClose = () => {
     setParams([]);
     setRunSettingsAreOpen(false);
+    setFileName('');
+  }
+
+  const handleScheduleClose = () => {
+    setParams([]);
+    setScheduleSettingsAreOpen(false);
     setFileName('');
   }
 
@@ -39,11 +58,19 @@ export const Recordings = ({ handleEditRecording, handleRunRecording, setFileNam
         isTask={params.length !== 0}
         params={params}
       />
+ <ScheduleSettingsModal isOpen={scheduleSettingsAreOpen}
+        handleClose={handleScheduleClose}
+        handleStart={(settings) => handleScheduleRecording(settings)}
+        isTask={params.length !== 0}
+        params={params}
+      />
+
       <Grid container direction="column" sx={{ padding: '30px' }}>
         <Grid item xs>
           <RecordingsTable
             handleEditRecording={handleEditRecording}
             handleRunRecording={handleSettingsAndRun}
+            handleScheduleRecording={handleSettingsAndSchedule}
           />
         </Grid>
       </Grid>
