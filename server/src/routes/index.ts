454938c768a80fe80aa2eeaad18ee459fe90ee7c
karishmas6 <carishmashukla28@gmail.com>
2024-09-15 17:56:27 +0530

    feat: export auth router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { router as record } from './record';
 import { router as workflow } from './workflow';
 import { router as storage } from './storage';
+import { router as auth } from './auth';
 
 export {
     record,
     workflow,
     storage,
+    auth,
 };
