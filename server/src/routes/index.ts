60b8dc134f54dbffbc2f3374c7eb1c96d186a0cd
karishmas6 <carishmashukla28@gmail.com>
2024-09-17 21:00:13 +0530

    feat: global export integration

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,12 @@ import { router as record } from './record';
 import { router as workflow } from './workflow';
 import { router as storage } from './storage';
 import { router as auth } from './auth';
+import { router as integration } from './integration';
 
 export {
     record,
     workflow,
     storage,
     auth,
+    integration,
 };
