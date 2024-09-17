84a4afa7fc7ef6b55a3f3debb88bbd407fe5545e
karishmas6 <carishmashukla28@gmail.com>
2024-09-17 20:39:42 +0530

    feat: upload service account credentials

--- server/src/routes/integration.ts
+++ server/src/routes/integration.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';;
 import { google, sheets_v4 } from "googleapis";
+import fs from 'fs';
+import path from 'path';
 
 export const router = Router()
 
@@ -10,3 +12,9 @@ router.post('/upload-credentials', (req, res) => {
     if (!credentials) {
       return res.status(400).json({ message: 'Credentials are required.' });
     }
+    // Todo: Store the credentials in a secure place (for test, we store them locally)
+  const storedCredentialsPath = path.join(__dirname, 'service_account_credentials.json');
+  fs.writeFileSync(storedCredentialsPath, JSON.stringify(credentials));
+
+  res.status(200).json({ message: 'Service Account credentials saved successfully.' });
+});
