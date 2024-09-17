6312753d77185b16ccba63910d4fe0e5e9bb536a
karishmas6 <carishmashukla28@gmail.com>
2024-09-17 22:36:40 +0530

    feat: handle upload credentials

---
+++ src/api/interpretation.ts
@@ -0,0 +1,13 @@
+import { default as axios } from "axios";
+
+const handleUploadCredentials = async (credentials: any) => {
+    try {
+      await axios.post('http://localhost:8080/integration/upload-credentials', { credentials: JSON.parse(credentials) });
+      alert('Service Account credentials saved successfully.');
+    } catch (error) {
+      console.error('Error uploading credentials:', error);
+      alert('Failed to upload credentials.');
+    }
+  };
+
+
