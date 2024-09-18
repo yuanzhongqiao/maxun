dd838016ba84c5be432b13ef4afcc61f0f216ff3
karishmas6 <carishmashukla28@gmail.com>
2024-09-18 18:30:04 +0530

    feat: -rm handleWriteToSheet

--- src/api/interpretation.ts
+++ src/api/interpretation.ts
@@ -10,14 +10,3 @@ export const handleUploadCredentials = async (credentials: any) => {
         alert('Failed to upload credentials.');
     }
 };
-
-export const handleWriteToSheet = async (spreadsheetId: any, range: any) => {
-    try {
-        await axios.post('http://localhost:8080/integration/write-to-sheet', { spreadsheetId, range });
-        alert('Data written to Google Sheet successfully.');
-    } catch (error) {
-        console.error('Error writing to Google Sheet:', error);
-        alert('Failed to write to Google Sheet.');
-    }
-};
-
