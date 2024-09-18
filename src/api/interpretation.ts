14ef8b9750dcd39d16b51f02422ee1252c13a7e5
karishmas6 <carishmashukla28@gmail.com>
2024-09-18 22:32:16 +0530

    feat: !alerts

--- src/api/interpretation.ts
+++ src/api/interpretation.ts
@@ -5,14 +5,12 @@ export const handleUploadCredentials = async (fileName: any, credentials: any, s
     try {
         const response = await axios.post('http://localhost:8080/integration/upload-credentials', { fileName, credentials: JSON.parse(credentials), spreadsheetId, range });
         if (response.status === 200) {
-            alert('Service Account credentials saved successfully.');
             return response.data;
           } else {
             throw new Error(`Couldn't make gsheet integration for ${fileName}`);
         }
     } catch (error) {
         console.error('Error uploading credentials:', error);
-        alert('Failed to upload credentials.');
         return false;
     }
 };
