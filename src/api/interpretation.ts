a1d672847f5158018e6f56b0d431f2305d9bfd93
karishmas6 <carishmashukla28@gmail.com>
2024-09-18 20:01:45 +0530

    feat: pass fileName

--- src/api/interpretation.ts
+++ src/api/interpretation.ts
@@ -1,9 +1,9 @@
 import { default as axios } from "axios";
 
 // todo: proper typescript types for params
-export const handleUploadCredentials = async (credentials: any, spreadsheetId: any, range: any) => {
+export const handleUploadCredentials = async (fileName: any, credentials: any, spreadsheetId: any, range: any) => {
     try {
-        await axios.post('http://localhost:8080/integration/upload-credentials', { credentials: JSON.parse(credentials), spreadsheetId, range });
+        await axios.post('http://localhost:8080/integration/upload-credentials', { fileName, credentials: JSON.parse(credentials), spreadsheetId, range });
         alert('Service Account credentials saved successfully.');
     } catch (error) {
         console.error('Error uploading credentials:', error);
