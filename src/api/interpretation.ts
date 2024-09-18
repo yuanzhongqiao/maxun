d40ec715b2601b94aa67479c02aec9553881dd3d
karishmas6 <carishmashukla28@gmail.com>
2024-09-18 22:31:48 +0530

    feat: return promise

--- src/api/interpretation.ts
+++ src/api/interpretation.ts
@@ -1,12 +1,18 @@
 import { default as axios } from "axios";
 
 // todo: proper typescript types for params
-export const handleUploadCredentials = async (fileName: any, credentials: any, spreadsheetId: any, range: any) => {
+export const handleUploadCredentials = async (fileName: any, credentials: any, spreadsheetId: any, range: any): Promise<boolean> => {
     try {
-        await axios.post('http://localhost:8080/integration/upload-credentials', { fileName, credentials: JSON.parse(credentials), spreadsheetId, range });
-        alert('Service Account credentials saved successfully.');
+        const response = await axios.post('http://localhost:8080/integration/upload-credentials', { fileName, credentials: JSON.parse(credentials), spreadsheetId, range });
+        if (response.status === 200) {
+            alert('Service Account credentials saved successfully.');
+            return response.data;
+          } else {
+            throw new Error(`Couldn't make gsheet integration for ${fileName}`);
+        }
     } catch (error) {
         console.error('Error uploading credentials:', error);
         alert('Failed to upload credentials.');
+        return false;
     }
 };
