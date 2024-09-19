08e149b4e8bfcc1bcf567feef1dc2ae8cb8bb6f6
karishmas6 <carishmashukla28@gmail.com>
2024-09-19 18:26:51 +0530

    feat: remove handleWriteToGSheet

--- src/api/integration.ts
+++ src/api/integration.ts
@@ -14,20 +14,3 @@ export const handleUploadCredentials = async (fileName: any, credentials: any, s
         return false;
     }
 };
-
-export const handleWriteToGsheet = async (fileName: string, runId: string): Promise<{
-    success: boolean;
-    message: string;
-}> => {
-    try {
-        const response = await axios.post(`http://localhost:8080/integration/update-google-sheet/${fileName}/${runId}`);
-        if (response.status === 200) {
-            return response.data;
-        } else {
-            throw new Error(`Couldn't make gsheet integration for ${fileName}`);
-        }
-    } catch (error) {
-        console.error('Error uploading credentials:', error);
-        return { success: false, message: 'Failed to write to Google Sheet' };
-    }
-};
