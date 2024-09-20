95985a9d8544ae3236d96fefc6434038cc9747ed
Naveen Pandey <naveenpandeytech@gmail.com>
2024-09-20 17:07:41 +0530

    feat: add proper types for params

--- src/api/integration.ts
+++ src/api/integration.ts
@@ -1,7 +1,7 @@
 import { default as axios } from "axios";
 
 // todo: proper typescript types for params
-export const handleUploadCredentials = async (fileName: any, credentials: any, spreadsheetId: any, range: any): Promise<boolean> => {
+export const handleUploadCredentials = async (fileName: string, credentials: any, spreadsheetId: string, range: string): Promise<boolean> => {
     try {
         const response = await axios.post('http://localhost:8080/integration/upload-credentials', { fileName, credentials: JSON.parse(credentials), spreadsheetId, range });
         if (response.status === 200) {
