dfbc5470e64ce5a1b8d3fd1e249d7ea612c0c278
Naveen Pandey <naveenpandeytech@gmail.com>
2024-09-20 17:08:09 +0530

    chore: remove todo

--- src/api/integration.ts
+++ src/api/integration.ts
@@ -1,6 +1,5 @@
 import { default as axios } from "axios";
 
-// todo: proper typescript types for params
 export const handleUploadCredentials = async (fileName: string, credentials: any, spreadsheetId: string, range: string): Promise<boolean> => {
     try {
         const response = await axios.post('http://localhost:8080/integration/upload-credentials', { fileName, credentials: JSON.parse(credentials), spreadsheetId, range });
