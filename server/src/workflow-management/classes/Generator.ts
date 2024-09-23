83da2b265f0b296201508a35154b1d710a2f5def
karishmas6 <carishmashukla28@gmail.com>
2024-09-23 17:25:39 +0530

    feat: define default values

--- server/src/workflow-management/classes/Generator.ts
+++ server/src/workflow-management/classes/Generator.ts
@@ -99,6 +99,8 @@ export class WorkflowGenerator {
     lastUsedSelector: '',
     lastIndex: null,
     lastAction: '',
+    lastUsedSelectorTagName: '',
+    lastUsedSelectorInnerText: '',
   }
 
   /**
