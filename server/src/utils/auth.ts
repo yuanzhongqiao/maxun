4186c9572e1dfdfd95c4a7029e269630003f6dd4
karishmas6 <carishmashukla28@gmail.com>
2024-09-23 23:44:21 +0530

    feat: hash password

---
+++ server/src/utils/auth.ts
@@ -0,0 +1,24 @@
+import bcrypt from "bcrypt";
+
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(12, (err, salt) => {
+            if (err) {
+                reject(err)
+            }
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(hash)
+            })
+        })
+    })
+}
+
+// password from frontend and hash from database
+const comparePassword = (password, hash) => {
+    return bcrypt.compare(password, hash)
+}
+
+module.exports = { hashPassword, comparePassword }
