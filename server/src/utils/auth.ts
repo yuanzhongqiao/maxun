a2e4c2dde086c8a336164f16433c6abd6be3c57e
karishmas6 <carishmashukla28@gmail.com>
2024-09-24 19:16:51 +0530

    fix: ts errors

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 
-export const hashPassword = (password) => {
+export const hashPassword = (password: any) => {
     return new Promise((resolve, reject) => {
         bcrypt.genSalt(12, (err, salt) => {
             if (err) {
@@ -17,6 +17,6 @@ export const hashPassword = (password) => {
 }
 
 // password from frontend and hash from database
-export const comparePassword = (password, hash) => {
+export const comparePassword = (password: any, hash: any) => {
     return bcrypt.compare(password, hash)
 }
