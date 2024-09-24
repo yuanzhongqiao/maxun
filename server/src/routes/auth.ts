1bcc8d372b811540a23e2ee63e46137ee8d81de7
karishmas6 <carishmashukla28@gmail.com>
2024-09-24 17:48:01 +0530

    chore: remove unused import

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response } from 'express';
-import { hashPassword, comparePassword } from '../utils/auth';
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
 export const router = Router();
