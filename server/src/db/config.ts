24881daa6e4b62510fb6d69ae4d4c35739127a27
karishmas6 <carishmashukla28@gmail.com>
2024-09-24 17:24:24 +0530

    chore: lint

--- server/src/db/config.ts
+++ server/src/db/config.ts
@@ -4,23 +4,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-  }
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASSWORD as string,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+    }
 );
 
 export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connected successfully');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 };
 
 export default sequelize;
