6bea5a2ad145d4bff040c0878679863a70690e65
karishmas6 <carishmashukla28@gmail.com>
2024-09-25 18:08:35 +0530

    chore: lint

--- server/src/db/config.ts
+++ server/src/db/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const sequelize = new Sequelize(
-   `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     {
         host: 'localhost',
         dialect: 'postgres',
