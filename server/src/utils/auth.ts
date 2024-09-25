037c7b597f8bcd2605c5f2f16f0b2670d43ecaec
karishmas6 <carishmashukla28@gmail.com>
2024-09-25 18:43:43 +0530

    feat: return Promise<string?

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 
-export const hashPassword = (password: string) => {
+export const hashPassword = (password: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         bcrypt.genSalt(12, (err, salt) => {
             if (err) {
