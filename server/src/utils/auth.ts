8d29edc6345a3103ac3bb859f1d04940bd196e28
karishmas6 <carishmashukla28@gmail.com>
2024-09-25 18:43:09 +0530

    feat: set password as string

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 
-export const hashPassword = (password: any) => {
+export const hashPassword = (password: string) => {
     return new Promise((resolve, reject) => {
         bcrypt.genSalt(12, (err, salt) => {
             if (err) {
@@ -17,6 +17,6 @@ export const hashPassword = (password: any) => {
 }
 
 // password from frontend and hash from database
-export const comparePassword = (password: any, hash: any) => {
+export const comparePassword = (password: string, hash: any) => {
     return bcrypt.compare(password, hash)
 }
