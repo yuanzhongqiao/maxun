91a69022cb8bac3c80d225a37789d258b1eb1c58
karishmas6 <carishmashukla28@gmail.com>
2024-09-26 17:47:26 +0530

    feat: requireAPIKey middleware

---
+++ server/src/middlewares/api.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+
+export const requireAPIKey = async (req: Request, res: Response, next: any) => {
+    const apiKey = req.headers['x-api-key'];
+    if (!apiKey) {
+        return res.status(401).json({ error: "API key is missing" });
+    }
+    const user = await User.findOne({ where: { api_key: apiKey } });
+    if (!user) {
+        return res.status(403).json({ error: "Invalid API key" });
+    }
+    req.user = user;  
+    next();  
+};
