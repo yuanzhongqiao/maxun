c379b849428aad4c7e40c07099c82b5cab09d87a
karishmas6 <carishmashukla28@gmail.com>
2024-10-02 22:39:09 +0530

    feat: proxy password validation

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -54,6 +54,13 @@ User.init(
         proxy_username: {
             type: DataTypes.STRING,
             allowNull: true,
+            validate: {
+                isProxyPasswordRequired(value: string | null) {
+                    if (value && !this.proxy_password) {
+                        throw new Error('Proxy password is required when proxy username is provided');
+                    }
+                },
+            },
         },
         proxy_password: {
             type: DataTypes.STRING,
