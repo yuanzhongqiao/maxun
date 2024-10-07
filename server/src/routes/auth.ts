86ec6920aa2f7e6504b94c6b366c619c5e878175
karishmas6 <carishmashukla28@gmail.com>
2024-10-07 18:50:34 +0530

    feat: set expiresIn to 12h for login

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -47,7 +47,7 @@ router.post('/login', async (req, res) => {
         const match = await comparePassword(password, user.password)
         if (!match) return res.status(400).send('Invalid email or password')
 
-        const token = jwt.sign({ id: user?.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user?.id }, process.env.JWT_SECRET as string, { expiresIn: '12h' });
 
         // return user and token to client, exclude hashed password
         if (user) {
