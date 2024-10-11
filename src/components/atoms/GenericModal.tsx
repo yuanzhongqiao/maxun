63bc4749cf79175e5d19cc6655aa41666b79d0bb
karishmas6 <carishmashukla28@gmail.com>
2024-10-11 18:23:09 +0530

    fix: revert to width 500

--- src/components/atoms/GenericModal.tsx
+++ src/components/atoms/GenericModal.tsx
@@ -33,7 +33,7 @@ const defaultModalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 300,
+  width: 500,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
