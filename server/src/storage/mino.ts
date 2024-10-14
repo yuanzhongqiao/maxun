8ae2b9f21fa1d2c6a962ae785cca649ae421a69b
karishmas6 <carishmashukla28@gmail.com>
2024-10-14 23:48:31 +0530

    feat: setup minio client

---
+++ server/src/storage/mino.ts
@@ -0,0 +1,9 @@
+import { Client } from 'minio';
+
+const minioClient = new Client({
+  endPoint: process.env.MINIO_ENDPOINT || 'localhost',
+  port: parseInt(process.env.MINIO_PORT || '9000'),
+  useSSL: false,
+  accessKey: process.env.MINIO_ACCESS_KEY || 'minio-access-key',
+  secretKey: process.env.MINIO_SECRET_KEY || 'minio-secret-key',
+});
