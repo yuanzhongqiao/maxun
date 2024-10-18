7a1f3be2ebb7aedb06712f7203480f359c6b3458
karishmas6 <carishmashukla28@gmail.com>
2024-10-18 16:49:34 +0530

    feat: set width 900, height 500

--- src/components/organisms/BrowserWindow.tsx
+++ src/components/organisms/BrowserWindow.tsx
@@ -64,7 +64,7 @@ export const BrowserWindow = () => {
     const [paginationSelector, setPaginationSelector] = useState<string>('');
 
     const { socket } = useSocketStore();
-    const { width, height } = useBrowserDimensionsStore();
+    //const { width, height } = useBrowserDimensionsStore();
     const { getText, getList, paginationMode, paginationType, limitMode } = useActionContext();
     const { addTextStep, addListStep } = useBrowserSteps();
 
@@ -316,7 +316,7 @@ export const BrowserWindow = () => {
 
 
     return (
-        <div onClick={handleClick}>
+        <div onClick={handleClick} style={{ width: '900px', height: '500px'}} id="browser-window">
             {
                 getText === true || getList === true ? (
                     <GenericModal
@@ -360,15 +360,15 @@ export const BrowserWindow = () => {
                 <Highlighter
                     unmodifiedRect={highlighterData?.rect}
                     displayedSelector={highlighterData?.selector}
-                    width={width}
-                    height={height}
+                    width={900}
+                    height={500}
                     canvasRect={canvasRef.current.getBoundingClientRect()}
                 />
                 : null}
             <Canvas
                 onCreateRef={setCanvasReference}
-                width={width}
-                height={height}
+                width={900}
+                height={500}
             />
         </div>
     );
@@ -383,7 +383,7 @@ const drawImage = (image: string, canvas: HTMLCanvasElement): void => {
     img.src = image;
     img.onload = () => {
         URL.revokeObjectURL(img.src);
-        ctx?.drawImage(img, 0, 0, 1280, 720);
+        ctx?.drawImage(img, 0, 0, 900, 500);
     };
 
 };
