da2368aaaa222ddab864b5ae526a9fbf5b5de232
karishmas6 <carishmashukla28@gmail.com>
2024-10-19 09:44:00 +0530

    chore: revert

--- src/pages/RecordingPage.tsx
+++ src/pages/RecordingPage.tsx
@@ -32,6 +32,7 @@ export const RecordingPage = ({ recordingName }: RecordingPageProps) => {
     pair: null,
     index: 0,
   });
+  const [showOutputData, setShowOutputData] = useState(false);
 
   const browserContentRef = React.useRef<HTMLDivElement>(null);
   const workflowListRef = React.useRef<HTMLDivElement>(null);
@@ -40,6 +41,9 @@ export const RecordingPage = ({ recordingName }: RecordingPageProps) => {
   const { setWidth } = useBrowserDimensionsStore();
   const { browserId, setBrowserId, recordingId } = useGlobalInfoStore();
 
+  const handleShowOutputData = useCallback(() => {
+    setShowOutputData(true);
+  }, []);
 
   const handleSelectPairForEdit = (pair: WhereWhatPair, index: number) => {
     setPairForEdit({
