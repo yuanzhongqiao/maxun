fac1eab6588599e0e2277fa79c71041136c71d8d
karishmas6 <carishmashukla28@gmail.com>
2024-10-19 18:44:52 +0530

    feat: remove setShowOutputData prop

--- src/components/organisms/RightSidePanel.tsx
+++ src/components/organisms/RightSidePanel.tsx
@@ -40,10 +40,9 @@ const fetchWorkflow = (id: string, callback: (response: WorkflowFile) => void) =
 // 2. Handle non custom action steps
 interface RightSidePanelProps {
   onFinishCapture: () => void;
-  setShowOutputData: (show: boolean) => void;
 }
 
-export const RightSidePanel: React.FC<RightSidePanelProps> = ({ onFinishCapture, setShowOutputData }) => {
+export const RightSidePanel: React.FC<RightSidePanelProps> = ({ onFinishCapture }) => {
   const [workflow, setWorkflow] = useState<WorkflowFile>(emptyWorkflow);
   const [textLabels, setTextLabels] = useState<{ [id: string]: string }>({});
   const [errors, setErrors] = useState<{ [id: string]: string }>({});
