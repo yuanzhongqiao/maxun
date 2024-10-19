d2f06fc8e3757845888e0ffc737f8be2c291ee07
karishmas6 <carishmashukla28@gmail.com>
2024-10-19 14:00:05 +0530

    feat: remove interpretation log

--- src/components/organisms/BrowserContent.tsx
+++ src/components/organisms/BrowserContent.tsx
@@ -132,7 +132,6 @@ export const BrowserContent = () => {
         handleUrlChanged={handleUrlChanged}
       />
       <BrowserWindow/>
-      <InterpretationLog isOpen={showOutputData} setIsOpen={setShowOutputData} />
     </div>
   );
 }
