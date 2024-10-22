81dfa47040e42fed799ce3ff7259db90ba5f67b0
karishmas6 <carishmashukla28@gmail.com>
2024-10-23 00:13:40 +0530

    chore: lint

--- src/components/molecules/ScheduleSettings.tsx
+++ src/components/molecules/ScheduleSettings.tsx
@@ -136,105 +136,105 @@ export const ScheduleSettingsModal = ({ isOpen, handleStart, handleClose, initia
           {
             (schedule != null) ? (
               <Box mt={2} display="flex" justifyContent="space-between">
-          <Button
-            onClick={deleteRobotSchedule}
-            variant="outlined"
-            color="secondary"
-          >
-            Delete Schedule
-          </Button>
-        </Box>
+                <Button
+                  onClick={deleteRobotSchedule}
+                  variant="outlined"
+                  color="secondary"
+                >
+                  Delete Schedule
+                </Button>
+              </Box>
             ) : (
               <>
-              <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-            <Typography sx={{ marginRight: '10px' }}>Run once every</Typography>
-            <TextField
-              type="number"
-              value={settings.runEvery}
-              onChange={(e) => handleChange('runEvery', parseInt(e.target.value))}
-              sx={textStyle}
-              inputProps={{ min: 1 }}
-            />
-            <Dropdown
-              label=""
-              id="runEveryUnit"
-              value={settings.runEveryUnit}
-              handleSelect={(e) => handleChange('runEveryUnit', e.target.value)}
-              sx={dropDownStyle}
-            >
-              {units.map((unit) => (
-                <MenuItem key={unit} value={unit}>{unit}</MenuItem>
-              ))}
-            </Dropdown>
-          </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                  <Typography sx={{ marginRight: '10px' }}>Run once every</Typography>
+                  <TextField
+                    type="number"
+                    value={settings.runEvery}
+                    onChange={(e) => handleChange('runEvery', parseInt(e.target.value))}
+                    sx={textStyle}
+                    inputProps={{ min: 1 }}
+                  />
+                  <Dropdown
+                    label=""
+                    id="runEveryUnit"
+                    value={settings.runEveryUnit}
+                    handleSelect={(e) => handleChange('runEveryUnit', e.target.value)}
+                    sx={dropDownStyle}
+                  >
+                    {units.map((unit) => (
+                      <MenuItem key={unit} value={unit}>{unit}</MenuItem>
+                    ))}
+                  </Dropdown>
+                </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-            <Typography sx={{ marginBottom: '5px', marginRight: '25px' }}>Start from / On</Typography>
-            <Dropdown
-              label=""
-              id="startFrom"
-              value={settings.startFrom}
-              handleSelect={(e) => handleChange('startFrom', e.target.value)}
-              sx={dropDownStyle}
-            >
-              {days.map((day) => (
-                <MenuItem key={day} value={day}>{day}</MenuItem>
-              ))}
-            </Dropdown>
-          </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                  <Typography sx={{ marginBottom: '5px', marginRight: '25px' }}>Start from / On</Typography>
+                  <Dropdown
+                    label=""
+                    id="startFrom"
+                    value={settings.startFrom}
+                    handleSelect={(e) => handleChange('startFrom', e.target.value)}
+                    sx={dropDownStyle}
+                  >
+                    {days.map((day) => (
+                      <MenuItem key={day} value={day}>{day}</MenuItem>
+                    ))}
+                  </Dropdown>
+                </Box>
 
-          {['MINUTES', 'HOURS'].includes(settings.runEveryUnit) ? (
-            <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-              <Box sx={{ marginRight: '20px' }}>
-                <Typography sx={{ marginBottom: '5px' }}>In Between</Typography>
-                <TextField
-                  type="time"
-                  value={settings.atTimeStart}
-                  onChange={(e) => handleChange('atTimeStart', e.target.value)}
-                  sx={textStyle}
-                />
-                <TextField
-                  type="time"
-                  value={settings.atTimeEnd}
-                  onChange={(e) => handleChange('atTimeEnd', e.target.value)}
-                  sx={textStyle}
-                />
-              </Box>
-            </Box>
-          ) : (
-            <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-              <Typography sx={{ marginBottom: '5px', marginRight: '10px' }}>At Around</Typography>
-              <TextField
-                type="time"
-                value={settings.atTimeStart}
-                onChange={(e) => handleChange('atTimeStart', e.target.value)}
-                sx={textStyle}
-              />
-            </Box>
-          )}
+                {['MINUTES', 'HOURS'].includes(settings.runEveryUnit) ? (
+                  <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                    <Box sx={{ marginRight: '20px' }}>
+                      <Typography sx={{ marginBottom: '5px' }}>In Between</Typography>
+                      <TextField
+                        type="time"
+                        value={settings.atTimeStart}
+                        onChange={(e) => handleChange('atTimeStart', e.target.value)}
+                        sx={textStyle}
+                      />
+                      <TextField
+                        type="time"
+                        value={settings.atTimeEnd}
+                        onChange={(e) => handleChange('atTimeEnd', e.target.value)}
+                        sx={textStyle}
+                      />
+                    </Box>
+                  </Box>
+                ) : (
+                  <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                    <Typography sx={{ marginBottom: '5px', marginRight: '10px' }}>At Around</Typography>
+                    <TextField
+                      type="time"
+                      value={settings.atTimeStart}
+                      onChange={(e) => handleChange('atTimeStart', e.target.value)}
+                      sx={textStyle}
+                    />
+                  </Box>
+                )}
 
-          <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-            <Typography sx={{ marginRight: '10px' }}>Timezone</Typography>
-            <Dropdown
-              label=""
-              id="timezone"
-              value={settings.timezone}
-              handleSelect={(e) => handleChange('timezone', e.target.value)}
-              sx={dropDownStyle}
-            >
-              {validMomentTimezones.map((tz) => (
-                <MenuItem key={tz} value={tz}>{tz}</MenuItem>
-              ))}
-            </Dropdown>
-          </Box>
-          <Box mt={2} display="flex" justifyContent="flex-end">
-          <Button onClick={() => handleStart(settings)} variant="contained" color="primary">
-            Save Schedule
-          </Button>
-          <Button onClick={handleClose} color="primary" variant="outlined" style={{ marginLeft: '10px' }}>
-            Cancel
-          </Button>
-        </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                  <Typography sx={{ marginRight: '10px' }}>Timezone</Typography>
+                  <Dropdown
+                    label=""
+                    id="timezone"
+                    value={settings.timezone}
+                    handleSelect={(e) => handleChange('timezone', e.target.value)}
+                    sx={dropDownStyle}
+                  >
+                    {validMomentTimezones.map((tz) => (
+                      <MenuItem key={tz} value={tz}>{tz}</MenuItem>
+                    ))}
+                  </Dropdown>
+                </Box>
+                <Box mt={2} display="flex" justifyContent="flex-end">
+                  <Button onClick={() => handleStart(settings)} variant="contained" color="primary">
+                    Save Schedule
+                  </Button>
+                  <Button onClick={handleClose} color="primary" variant="outlined" style={{ marginLeft: '10px' }}>
+                    Cancel
+                  </Button>
+                </Box>
               </>
             )
           }
