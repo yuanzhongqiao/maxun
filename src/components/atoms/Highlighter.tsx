d232f37197df911febe70974392217afc02da94a
karishmas6 <carishmashukla28@gmail.com>
2024-10-24 00:06:33 +0530

    feat: use pink instead of red

--- src/components/atoms/Highlighter.tsx
+++ src/components/atoms/Highlighter.tsx
@@ -50,7 +50,7 @@ const HighlighterOutline = styled.div<HighlighterOutlineProps>`
   pointer-events: none !important;
   position: fixed !important;
   background: #ff5d5b26 !important;
-  outline: 4px solid red !important;
+  outline: 4px solid #ff00c3 !important;
   //border: 4px solid #ff5d5b !important;
   z-index: 2147483647 !important;
   //border-radius: 5px;
