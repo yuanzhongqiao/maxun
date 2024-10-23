dd1177bbb01d700d567398f999f89566da67c19e
karishmas6 <carishmashukla28@gmail.com>
2024-10-24 04:11:09 +0530

    feat: show run logs

--- src/components/molecules/RunContent.tsx
+++ src/components/molecules/RunContent.tsx
@@ -51,7 +51,7 @@ export const RunContent = ({ row, currentLog, interpretationInProgress, logEndRe
       <TabContext value={tab}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={tab} onChange={(e, newTab) => setTab(newTab)} aria-label="run-content-tabs">
-            {/* <Tab label="Log" value='log' /> */}
+            <Tab label="Log" value='log' />
             <Tab label="Input" value='input' />
             <Tab label="Output Data" value='output' />
           </Tabs>
