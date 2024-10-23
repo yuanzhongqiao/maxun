c578114e985aadf650ab5f0e9f83a49766bcd536
karishmas6 <carishmashukla28@gmail.com>
2024-10-24 04:40:05 +0530

    feat: import highlightjs styles

--- src/components/molecules/RunContent.tsx
+++ src/components/molecules/RunContent.tsx
@@ -15,6 +15,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import 'highlight.js/styles/github.css';
 
 interface RunContentProps {
   row: Data,
