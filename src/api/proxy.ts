0794dd81314624338c71e1bd9d47d8edab483b87
karishmas6 <carishmashukla28@gmail.com>
2024-10-26 05:32:44 +0530

    feat: get proxy config

--- src/api/proxy.ts
+++ src/api/proxy.ts
@@ -13,3 +13,17 @@ export const sendProxyConfig = async (proxyConfig: { server_url: string, usernam
         return false;
     }
 }
+
+export const getProxyConfig = async (): Promise<{ proxy_url: string, auth: boolean }> => {
+    try {
+        const response = await axios.get(`http://localhost:8080/proxy/config`);
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            throw new Error(`Failed to fetch proxy configuration. Try again.`);
+        }
+    } catch (error: any) {
+        console.log(error);
+        return { proxy_url: '', auth: false };
+    }
+}
