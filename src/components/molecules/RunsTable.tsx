a2438a2e1833a28b68c9bf4350cfc9c5665f574d
karishmas6 <carishmashukla28@gmail.com>
2024-10-28 20:14:32 +0530

    feat: custom status

--- src/components/molecules/RunsTable.tsx
+++ src/components/molecules/RunsTable.tsx
@@ -16,7 +16,7 @@ import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface Column {
-  id: 'status' | 'name' | 'startedAt' | 'finishedAt' | 'runId' | 'delete' | 'runBy';
+  id: 'robotStatus' | 'name' | 'startedAt' | 'finishedAt' | 'runId' | 'delete' | 'runBy';
   label: string;
   minWidth?: number;
   align?: 'right';
@@ -24,7 +24,7 @@ interface Column {
 }
 
 export const columns: readonly Column[] = [
-  { id: 'status', label: 'Status', minWidth: 80 },
+  { id: 'robotStatus', label: 'Status', minWidth: 80 },
   { id: 'name', label: 'Robot Name', minWidth: 80 },
   { id: 'startedAt', label: 'Started at', minWidth: 80 },
   { id: 'finishedAt', label: 'Finished at', minWidth: 80 },
