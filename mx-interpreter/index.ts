389e8fd2f6b07bb33c36d88596d2fff5960b4184
karishmas6 <carishmashukla28@gmail.com>
2024-10-29 08:49:02 +0530

    chore: remove old files

--- mx-interpreter/index.ts
+++
@@ -1,8 +0,0 @@
-import Interpreter from './interpret';
-
-export default Interpreter;
-export { default as Preprocessor } from './preprocessor';
-export type {
-  WorkflowFile, WhereWhatPair, Where, What,
-} from './types/workflow';
-export { unaryOperators, naryOperators, meta as metaOperators } from './types/logic';
