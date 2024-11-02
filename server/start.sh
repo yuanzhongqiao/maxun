4b058eb50602a980489d504595a8a87ebfdc745b
karishmas6 <carishmashukla28@gmail.com>
2024-11-03 01:11:35 +0530

    chore: start server shell script

--- server/start.sh
+++ server/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start Xvfb in the background with the desired dimensions
-Xvfb :0 -screen 0 900x400x24 &
+#Xvfb :0 -screen 0 900x400x24 &
 
 # Wait for Xvfb to start
-sleep 2
+#sleep 2
 
 # Execute the Node.js application
 exec npm run server
